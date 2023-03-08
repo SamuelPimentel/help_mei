af8d342cc677958a0c1544ad0f7cd2db6685ae38
NatSilva <nat_silva385@outlook.com>
2023-03-08 16:28:46 -0300

    feat: adicionado: cores

---
+++ lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const periwinle = Color(0xFFB4ADEA);
+const periwinleLight = Color(0xFFC3BDEE);
+const perinwinkleDark = Color(0xFF7D79A3);
+
+const maize = Color(0xFFFFED65);
+const maizeLight = Color(0xFFFFF083);
+const maizeDark = Color(0xFFB2A546);
+
+const vanDyke = Color(0xFF484041);
+const vanDykePaper = Color(0xFF544c4e);
+const antiFlashWhite = Color(0xFFF3F3F4);
